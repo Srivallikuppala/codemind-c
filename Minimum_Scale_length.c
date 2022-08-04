3646886ea4225adbb7edce44950da4ead4edd4ec
Srivallikuppala <96306413+Srivallikuppala@users.noreply.github.com>
2022-08-04 21:09:22 +0530

    #include<stdio.h> int main() {     int n,i,hcf;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     hcf=arr[0];     int j=1;     while(j<n)     {         if(arr[j]%hcf==0)         {             j++;         }         else         {             hcf=arr[j]%hcf;         }     }     printf("%d",hcf);     return 0; }

---
+++ Minimum_Scale_length.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,hcf;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    hcf=arr[0];
+    int j=1;
+    while(j<n)
+    {
+        if(arr[j]%hcf==0)
+        {
+            j++;
+        }
+        else
+        {
+            hcf=arr[j]%hcf;
+        }
+    }
+    printf("%d",hcf);
+    return 0;
+}
